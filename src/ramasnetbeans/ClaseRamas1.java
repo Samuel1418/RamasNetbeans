c93dfec92aead7009aae6e8105200f9cab326240
Samuel1418 <sgomezpintos@danielcastelao.org>
2018-11-08 09:17:35 +0100

    Rama B

---
+++ src/ramasnetbeans/ClaseRamas1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramasnetbeans;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ClaseRamas1 {
+    public void hola(){
+        System.out.println("Esta es la rama B");
+        
+    }
+    
+}
